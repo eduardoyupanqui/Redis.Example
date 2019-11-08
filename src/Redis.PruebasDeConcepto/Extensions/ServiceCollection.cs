135254ab0cb9ef0c8fb49b5f035db476ce82df44
Eduardo Yupanqui <eduardo.yupanqui@outlook.com>
2019-11-08 17:41:25 -0500

    Configurando container IoC

---
+++ src/Redis.PruebasDeConcepto/Extensions/ServiceCollection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis.Extensions.Core;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Core.Implementations;
+using StackExchange.Redis.Extensions.Utf8Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Redis.PruebasDeConcepto.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            var redisConfiguration = configuration.GetSection("Redis").Get<RedisConfiguration>();
+            services.AddSingleton(redisConfiguration);
+            services.AddSingleton<IRedisCacheClient, RedisCacheClient>();
+            services.AddSingleton<IRedisCacheConnectionPoolManager, RedisCacheConnectionPoolManager>();
+            services.AddSingleton<IRedisDefaultCacheClient, RedisDefaultCacheClient>();
+            services.AddSingleton<ISerializer, Utf8JsonSerializer>();
+            return services;
+
+        }
+
+    }
+}
---
+++ src/Redis.PruebasDeConcepto/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Redis.PruebasDeConcepto.Extensions;
+using System;
+
+namespace Redis.PruebasDeConcepto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+             .AddJsonFile("appsettings.json", true, true)
+             .Build();
+            IServiceCollection services = new ServiceCollection();
+            services.AddRedis(config);
+
+
+
+            Console.WriteLine("Hello World!");
+
+
+
+
+        }
+
+        
+    }
+}
